14dbe49e3a9159704024b673f75148402fec0c48
Cameron Hiatt <50686686+Cameron-Hiatt@users.noreply.github.com>
2020-09-03 09:35:05 -0600

    Create Chapter 27 file

---
+++ Chapter 27/Exercise27_09.java
@@ -0,0 +1,27 @@
+/* 
+Author: Cameron Hiatt
+Date:	9-2-2020
+
+Given: "4.5"      Expected: 51451
+Given: "Hello"    Expected: 69609650
+*/
+public class Exercise27_09 { 
+  public static void main(String[] args) {
+    String s = "4.5";
+    System.out.println(hashCodeForString(s));
+    
+    s = "Hello";
+    System.out.println(hashCodeForString(s));
+  }
+  
+  public static int hashCodeForString(String s) {
+    
+	  int hashCode = 0;
+	  
+	  for(int i = 0; i < s.length(); i++)
+    {
+    	hashCode = 31 * hashCode + s.charAt(i);
+    }
+	  return hashCode;
+  }
+}
---
+++ Chapter 27/Exercise27_11.java
@@ -0,0 +1,31 @@
+//Author: Cameron Hiatt
+//Date: 9-3-2020
+
+//Changes made: I added lines 25-29 to complete the method
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<String>(); 
+    set.add("Smith");
+    set.add("Anderson");
+    set.add("Lewis");
+    set.add("Cook"); 
+    set.add("Smith");
+    
+    ArrayList<String> list = setToList(set);
+    System.out.println(list);
+  }
+  
+  public static <E> ArrayList<E> setToList(Set<E> s) {
+    
+	ArrayList<E> a = new ArrayList<>();
+   
+    a.addAll(s);
+   
+    return a;
+  }
+}
